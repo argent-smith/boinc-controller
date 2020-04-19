b2266a3950b416d23fd1cc859997c5efc2c553e4
Pavel Argentov <argentoff@gmail.com>
2020-04-20 01:48:06 +0300

    Beginning extracting a module for requests

--- protocol.ml
+++ protocol.ml
@@ -1,37 +1,53 @@
+open Lwt
 open Lwt.Infix
 
 module Commands (S : Mirage_stack.V4) = struct
   module TCP = S.TCPV4
   module IP = Ipaddr.V4
 
-  let request_state flow =
-    let open S.TCPV4 in
-    let request_text =
-      "<boinc_gui_request>\
-       <get_state/>\
-       </boinc_gui_request>\x03" in
-    let payload = Cstruct.of_string request_text in
-    write flow payload
-    >>= (
-      function
+  module StateRequest = struct
+    let query = "<get_state/>"
+
+    let query_wrapper =
+      Fmt.str "<boinc_gui_request>%s</boinc_gui_request>\x03"
+
+    let to_payload = Cstruct.of_string
+
+    let watch_write_or closer = function
       | Error err ->
-         Logs.err (fun f -> f "command transmission error: %a" pp_write_error err);
-         TCP.close flow
-      | _ -> Lwt.return_unit
-    )
-    >>= fun () -> read flow
-    >>= function | Error err ->
-                    Logs.err (fun f -> f "response receiving error: %a" pp_error err);
-                    TCP.close flow
-                 | Ok response -> (
-                   match response with
-                   | `Data payload -> Logs.debug (fun f -> f "node responded with payload:\n%s" (Cstruct.to_string payload))
-                   | `Eof -> Logs.debug (fun f -> f "EOF")
-                 );
-                 Lwt.return_unit
+         Logs.err (fun f -> f "command transmission error: %a" TCP.pp_write_error err);
+         closer ()
+      | _ -> return_unit
+
+    let get_result_or closer = function
+      | Error err ->
+         Logs.err (fun f -> f "response receiving error: %a" TCP.pp_error err);
+         closer ()
+         >>= (fun () -> return @@ Error ())
+      | Ok response ->
+         match response with
+         | `Data payload ->
+            let str = Cstruct.to_string payload in
+            Logs.debug (fun f -> f "node responded with payload:\n%s" str);
+            return @@ Ok str
+         | `Eof -> Logs.debug (fun f -> f "EOF"); return @@ Error ()
+
+    let send_to flow =
+      let open TCP in
+      let closer = (fun () -> close flow)
+      and reader = (fun () -> read flow)
+      and writer = write flow in
+      query_wrapper query
+      |> to_payload
+      |> writer
+      >>= watch_write_or closer
+      >>= reader
+      >>= get_result_or closer
+  end
 
   let state_ping flow =
     let addr, port = TCP.dst flow in
     Logs.debug (fun f -> f "pinging %s:%d" (IP.to_string addr) port);
-    request_state flow
+    StateRequest.send_to flow
+    >>= fun _ -> return_unit
 end
--- unikernel.ml
+++ unikernel.ml
@@ -1,7 +1,9 @@
+open Lwt
 open Lwt.Infix
 
 module BoincController (Time : Mirage_time.S) (S : Mirage_stack.V4) = struct
   module TCP = S.TCPV4
+  module IP = Ipaddr.V4
   module C = Protocol.Commands(S)
 
   let node_ip = Ipaddr.V4.make 192 168 47 51
@@ -9,31 +11,22 @@ module BoincController (Time : Mirage_time.S) (S : Mirage_stack.V4) = struct
 
   let start _time stack =
     let rec loop () =
+      let open TCP in
       let tcp = S.tcpv4 stack
       and duration = 1 in
-      TCP.create_connection tcp (node_ip, node_port)
+      Logs.debug (fun f -> f "connectiong to %s:%d" (IP.to_string node_ip) node_port);
+      create_connection tcp (node_ip, node_port)
       >>= (fun conn_result ->
         match conn_result with
         | Error err ->
            Logs.err (fun f -> f "connection error: %a" TCP.pp_error err);
-           TCP.disconnect tcp
+           disconnect tcp
         | Ok flow ->
            C.state_ping flow
-           >>= fun () -> TCP.close flow
+           >>= fun () -> close flow
       )
       >>= fun () -> Time.sleep_ns (Duration.of_sec duration)
       >>= fun () -> loop ()
     in
-    Lwt.join [loop ()]
+    join [loop ()]
 end
-
-(*
-
-module dependency plan
-
-BoincController -> Commands
-                -> CommandA
-                -> ...       -> functor Command (CD : CommandData) -> module type CommandData
-                -> CommandZ
-
-*)
